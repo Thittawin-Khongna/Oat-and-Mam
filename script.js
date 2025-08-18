957267c7a8da56d794119b6c118b208b627f3d19
ThittawinKhongna <ptkk00177@gmail.com>
2025-08-18 18:52:34 +0700

    edit10

--- script.js
+++ script.js
@@ -99,3 +99,24 @@
     moveTo(index + 1);
     }, 5000);
 
+    document.addEventListener("DOMContentLoaded", function() {
+        const fadeElements = document.querySelectorAll('.fade-in');
+
+        const observerOptions = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.2
+        };
+
+        const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('visible');
+                }
+            });
+        }, observerOptions);
+
+        fadeElements.forEach(element => {
+            observer.observe(element);
+        });
+    });
